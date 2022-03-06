d63885f85ab1b23757d37b0160681195ba44437d
Jonathon Quick <jonathon@spliff.li>
2022-03-06 18:49:01 +0100

    about to uninstall telethon and instead use tulir-telethon which fixes an authentication bug. This is the last commit before doing that

--- listen_for_trading_signals.py
+++ listen_for_trading_signals.py
@@ -1,5 +1,6 @@
 import configparser
 from telethon import TelegramClient, events
+from telethon.errors import SessionPasswordNeededError
 
 from parse_msg import parse_msg_signals
 from calc_trade_path import calc_trade_path, parse_trade_path_to_str
@@ -22,21 +23,22 @@ client = TelegramClient(username, api_id, api_hash)
 notification_channel_id = config['Telegram']['notification_channel_id']
 exchange = config['Bot Settings']['exchange']
 
-with client:
-      # '@C5543577423'  #
+
+async def main():
+    await client.start()
+    if not await client.is_user_authorized():
+        await client.send_code_request(phone)
+        try:
+            await client.sign_in(phone, input('Enter the code: '))
+        except SessionPasswordNeededError:
+            await client.sign_in(password=input('Password: '))
+
     notification_channel = client.get_entity(notification_channel_id)
-
-
-    # client.send_message(entity=entity, message="hello")
 
     @client.on(events.NewMessage(pattern=f'Your arbitrage alert {exchange} has been triggered!'))
     async def handler(event):
         # here received message, do something with event
         msg_str = event.to_dict()['message'].to_dict()['message']
-        # print(f"---------\n{msg_str}\n---------")  # check all possible methods/operations/attributes
-
-        # reply once and then disconnect
-        # reply = str(parse_msg_signals(msg_str))
 
         # TODO: confirm that signal is coming from specified channel ID, otherwise don't execute
 
@@ -50,11 +52,14 @@ with client:
                  f"\n{parse_trade_path_to_str(trade_path)}"
                  # f"\n{execute_trades(trade_path, signal_trades)}"
                 )
-        await event.reply(reply)
+
+        await client.send_message(entity=notification_channel_id, message=reply)
         trade_log = execute_trades(trade_path, signal_trades)
         await client.send_message(entity=notification_channel_id, message=trade_log)
         # await event.reply(trade_log)
         # await client.disconnect()
 
 
-    client.run_until_disconnected()
+with client:
+    client.run_until_disconnected(main())
+# client.run_until_disconnected()
